39a999b5c30417d7368c0a2ce35671fede63ac85
jonglar <romolo.porta3@gmail.com>
2021-01-11 22:01:23 +0100

    continue on menu-dei-dolci.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,25 @@
 
   http://www.imparareaprogrammare.it
 */
+const a = -5;
+const b = 8;
+const c = 4;
+const d = -2;
+
+let major = 0;
+let minor = 0;
+
+if (a>b) {
+  major = a;
+  minor = b;
+} else {
+  major = b;
+  minor = a;
+}
+
+if (c>major) {major=c};
+if (d>major) {major=d};
+if (c<minor) {minor=c};
+if (d<minor) {minor=d};
+
+console.log(`Major = ${major}, Minor = ${minor}`);
